e60c9a671f04580d67a2e35e19ad88ec0d258b6a
Pradeep <pradeepbanjare11@gmail.com>
2023-12-25 18:41:17 +0530

    Updates

---
+++ precache-manifest.65da3668178d51f7b77bd5d05de5bd2b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6c5e6f11e7e487cc8c19804b116bf2f",
+    "url": "/TextUtils-React/index.html"
+  },
+  {
+    "revision": "861eddfb1171ac786298",
+    "url": "/TextUtils-React/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8946a0edaa5c8cb07476",
+    "url": "/TextUtils-React/static/js/2.2a068b1b.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/TextUtils-React/static/js/2.2a068b1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8acca07efd9b80fc433",
+    "url": "/TextUtils-React/static/js/3.f665a10a.chunk.js"
+  },
+  {
+    "revision": "861eddfb1171ac786298",
+    "url": "/TextUtils-React/static/js/main.7ec6b91b.chunk.js"
+  },
+  {
+    "revision": "a865e17b624da34675a2",
+    "url": "/TextUtils-React/static/js/runtime-main.71fffba1.js"
+  }
+]);
